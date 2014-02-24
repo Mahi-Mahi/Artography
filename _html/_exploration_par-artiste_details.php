424a2258562423abed7b1b38125ea9ffb2ce8f84
Alexandre Cougard <alexandre.cougard@mahi-mahi.fr>
2014-02-24 12:53:46 +0100

    Création home page + optimisations images

--- _html/_exploration_par-artiste_details.php
+++ _html/_exploration_par-artiste_details.php
@@ -1,4 +1,5 @@
 <?php
+	$bodyclass = "";
 	include ("_header.php");
 ?>
 
---
+++ _html/_home.php
@@ -0,0 +1,43 @@
+<?php
+	$bodyclass = "home";
+	include ("_header.php");
+?>
+<div class="wrapper">
+
+	<div class="home-block">
+		<img src="images/visuel-hp-medium.png" alt="" />
+	</div><!-- .home-block -->
+
+	<div class="home-artist">
+		<h2>
+			<strong>Actuellement <br />111 artistes</strong> français <br />exposent dans<br /><strong>22 pays.</strong>
+		</h2>
+	</div><!-- .home-artist -->
+	<div class="home-galerie">
+		<h2>
+			<strong>En 2014 <br />145 galeries</strong><br /> françaises exposent<br /> dans <strong>22 pays.</strong>
+		</h2>
+	</div><!-- .home-galerie -->
+	<div class="news-block">
+		<h2>Actualités</h2>
+		<ul class="unstyled">
+			<li class="news-block-item"><a href="#"><strong>Materializing Six Years: Lucy R. Lippard and the Emergence of Conceptual Art</strong> @New York -  USA / 2 février - 3 mars 2014 </a></li>
+			<li class="news-block-item"><a href="#"><strong>Una ﬁnestra sul mondo - Da Dürer a Mondrian e oltre</strong> @New York -  USA / 2 février - 3 mars 2014 </a></li>
+		</ul>
+	</div><!-- .news-block -->
+	<div class="credits">
+		<ul class="unstyled">
+			<li class="credits-item"><a href="#" target="_blank"><img src="images/logo-ministere-affaires-etrangeres.png" class="logo-diplomatie" alt="" /></a></li>
+			<li class="credits-item"><a href="#" target="_blank"><img src="images/logo-institut-francais.png" alt="" /></a></li>
+		</ul>
+	</div><!-- .credits -->
+
+	<div class="nav-language">
+		<ul class="unstyled">
+			<li class="nav-language-item"><a href="#" class="active">Version française</a> | </li>
+			<li class="nav-language-item"><a href="#">English version</a></li>
+		</ul>
+	</div><!-- .nav-language -->
+
+</div><!-- .wrapper -->
+
